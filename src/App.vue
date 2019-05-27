9e4a0cea49cb419c8e93a1221ff242ccb8974a3c
shawn.thecool <shawn.thecool@kakaocorp.com>
2019-05-27 11:01:27 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  mounted() {
+    console.log('app mounted')
+  }
+}
+</script>
+
+<style>
+</style>
